ebed0c43a39d214e13a200ce7d4c7b62466c6269
Nannan Gu <396426364@qq.com>
2019-03-03 13:32:09 +1100

    new

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/vo/FilmConditionVO.java
@@ -0,0 +1,15 @@
+//package com.stylefeng.guns.rest.modular.film.vo;
+//
+//
+//import lombok.Data;
+//
+//import java.util.List;
+//
+//@Data
+//public class FilmConditionVO {
+//
+//    private List<CatVO> catInfo;
+//    private List<SourceVO> sourceInfo;
+//    private List<YearVO> yearInfo;
+//
+//}
